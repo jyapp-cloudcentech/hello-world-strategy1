2b436cb1df48d3a7447fc024637896e480d5bb1f
Justin Yapp <justin@cloudcentech.com>
2021-03-08 15:02:51 -0500

    Testing Features Integration

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `Hello World...From the ${process.env.ENVIRONMENT} environment`;
+    return `Hello World...From the ${process.env.ENVIRONMENT} environment. Testing Feature Integration`;
   }
 }
